f21abeafa8c766880cd2c9e190dc6a35b8a2f340
benjmnxu <benxu@seas.upenn.edu>
2024-12-17 16:16:57 -0500

    new dev/test/train

--- ben.py
+++ ben.py
@@ -1,15 +1,66 @@
 import pandas as pd
 
-train_path = "data/train.csv"
+# File paths
+# dev_path = "data/dev.csv"
+# train_path = "data/train.csv"
+# test_path = "data/test.csv"
+# synth_esh = "data/synthetic_ESH_1800.csv"
+# synth_nah = "data/synthetic_NAH_1800.csv"
+# synth_yta = "data/synthetic_YTA_1800.csv"
 
-data = pd.read_csv(train_path)
+# # Load data
+# dev_data = pd.read_csv(dev_path)
+# test_data = pd.read_csv(test_path)
+# train_data = pd.read_csv(train_path)
 
-# Print total number of target entries
-print("Total entries in target:", len(data["target"]))
+# synth_esh_data = pd.read_csv(synth_esh)
+# synth_nah_data = pd.read_csv(synth_nah)
+# synth_yta_data = pd.read_csv(synth_yta)
 
-# Print number of unique classes in target
-print("Number of unique classes in target:", data["target"].nunique())
+# # Add synthetic data to dev set (first 240 rows)
+# dev_data = pd.concat([
+#     dev_data,
+#     synth_esh_data.iloc[:240],
+#     synth_nah_data.iloc[:240],
+#     synth_yta_data.iloc[:240]
+# ], ignore_index=True)
 
-print("Number of unique classes in target:", data["target"].unique())
+# # Add synthetic data to test set (240 to 479 rows)
+# test_data = pd.concat([
+#     test_data,
+#     synth_esh_data.iloc[240:480],
+#     synth_nah_data.iloc[240:480],
+#     synth_yta_data.iloc[240:480]
+# ], ignore_index=True)
 
-print(data.iloc[0])
+# # Add synthetic data to train set (remaining rows)
+# train_data = pd.concat([
+#     train_data,
+#     synth_esh_data.iloc[480:],
+#     synth_nah_data.iloc[480:],
+#     synth_yta_data.iloc[480:]
+# ], ignore_index=True)
+
+# # Save new datasets
+# dev_data.to_csv("data/new_dev.csv", index=False)
+# test_data.to_csv("data/new_test.csv", index=False)
+# train_data.to_csv("data/new_train.csv", index=False)
+
+# Load new_dev_data
+new_dev_data = pd.read_csv("data/new_train.csv")
+
+# Get counts of each target label
+target_counts = new_dev_data['target'].value_counts()
+
+# Display specific counts
+nta_count = target_counts.get('NTA', 0)
+yta_count = target_counts.get('YTA', 0)
+nah_count = target_counts.get('NAH', 0)
+esh_count = target_counts.get('ESH', 0)
+
+# Print results
+print(f"NTA: {nta_count}")
+print(f"YTA: {yta_count}")
+print(f"NAH: {nah_count}")
+print(f"ESH: {esh_count}")
+
